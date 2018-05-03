b3de083deeca4815ff23b33077c81b3a7a2a4022
Martin Cupak <cupak.martin90@gmail.com>
2018-05-04 00:47:34 +0100

    Update entities.py

--- Data/entities.py
+++ Data/entities.py
@@ -5,25 +5,57 @@ from items import *
 shopkeep_dialogue = {
         'WELCOME' : 'Welcome to my shop. Look at my wares and please buy some.', 
         'LEAVE' : 'Bye adventurer! Please come again!', 
+        'GREET' : 'What was it, adventurer?',
         'DIALOGUE' : [
                 ['How are you?', 'I\'m fine'], 
                 ['What do you sell?', 'Just a sword and a potion.'],
                 ['Did you hear any gossips lately?', 'Ur mom.']
-]
-}
+]}
 
+npc_01_dia = {
+        'GREET' : 'Yes?',
+        'DIALOGUE' : [
+            ['Good day.', 'Good day to you, young man.']
+]}
+
+npc_02_dia = {
+        'GREET' : 'Hello.',
+        'DIALOGUE' : [
+            ['How is it going?', 'good good'],
+            ['Any gossips', 'Nope']
+        ]
+}
 # ======================= NPC ==============================
 
 class NPC:
-    def  __init__(self, name, dialogue = None):
+    def  __init__(self, xname, name, dialogue = None, quest = None, met = False):
+        self.xname = xname
         self.name = name
+        self.dialogue = dialogue
+        self.quest = quest
+        self.met = met
+
+    @property
+    def name(self):
+        if self.met == True:
+            return(self.xname)
+        return(self._name)
+
+    @name.setter
+    def name(self, val):
+        self._name = val
+    
+
+
+class Merchant(NPC):
+    def __init__(self, xname, name, dialogue = None, quest = None, met = False):
+        super().__init__(xname, name, dialogue, quest, met)
         self.weapons_inventory = [[dagger, 0], [short_sword, 0], [long_sword, 0], [two_handed_sword, 0], [axe, 0], [battleaxe, 0],
         [spear, 0]]
         self.potions_inventory = [[healing_potion, 0], [mana_potion, 0], [rejuvenation_potion, 0], [speed_potion, 0], [ironskin_potion, 0],
         [strength_potion, 0]]
         self.armors_inventory = [[clothes, 0], [leather_armor, 0], [studded_leather_armor, 0], [chain_mail_armor, 0], [plate_armor, 0],
         [mage_robe, 0], [master_robe, 0]]
-        self.dialogue = dialogue
 
     def npc_add_item(self, item, amount):
         for i in self.weapons_inventory:
@@ -47,8 +79,8 @@ class NPC:
             if i[0] == item:
                 i[1] -= 1
 
-shopkeep = NPC('Shopkeep', dialogue = shopkeep_dialogue)
 
+shopkeep = Merchant('Steve', 'Shopkeep', dialogue = shopkeep_dialogue)
 shopkeep.npc_add_item(dagger, 1)
 shopkeep.npc_add_item(short_sword, 1)
 shopkeep.npc_add_item(leather_armor, 1)
@@ -56,6 +88,9 @@ shopkeep.npc_add_item(studded_leather_armor, 1)
 shopkeep.npc_add_item(healing_potion, 3)
 shopkeep.npc_add_item(mana_potion, 3)
 shopkeep.npc_add_item(strength_potion, 3)
+
+npc_01 = NPC('Mary', 'Old lady', dialogue = npc_01_dia)
+npc_02 = NPC('Jim', 'small boy', dialogue = npc_02_dia)
 # ===================== ENEMIES ==============================
 
 class GiantRat:
